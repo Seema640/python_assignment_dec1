485c22ca8c37a29988b3dd0c2930444bf38da99b
seema <seema.tamang640@gmail.com>
2017-12-03 16:09:44 +0545

    birthday song for a friend

---
+++ birth_day_friend.py
@@ -0,0 +1,5 @@
+def birthday(name):
+    print("Happy birthday to you \n happy birthday dear {:s} \n happy birthday to you!!! May god bless you!!! ".format(name))
+
+friend = str(input("Enter your friend's name: "))
+birthday(friend)
